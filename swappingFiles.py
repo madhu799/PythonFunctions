e47f16c614ff0d3ac84013b9b88e1a45a25de764
MADDY <73730380+madhu799@users.noreply.github.com>
2021-11-08 18:05:31 +1100

    Add files via upload

---
+++ swappingFiles.py
@@ -0,0 +1,19 @@
+from os import write
+
+def swapFileData():
+    file1=input("Enter the first file path: ")
+    file2=input("Enter the second file path: ")
+    with open(file1,'r') as a:
+        data_a=a.read()
+    with open(file2,'r') as b:
+        data_b=b.read()
+    
+    with open(file1,'w+') as a:
+        a.write(data_b)
+    
+    with open(file2,'w+') as b:
+        b.write(data_a)
+
+swapFileData()
+
+print("Process Completed")
